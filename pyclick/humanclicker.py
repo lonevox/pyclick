879d701c6e7d78431ad98d9419442bc97e555ce6
lonevox <38600896+lonevox@users.noreply.github.com>
2020-05-16 19:37:05 +1200

    Added method "move_to_ellipse" in HumanClicker
    This method moves the mouse somewhere within an ellipse. The ellipse is defined by the ellipseOriginPoint, width, and height parameters. If you want the mouse to be moved only to the edge of the ellipse, then doRandomScale should be set to false.

--- pyclick/humanclicker.py
+++ pyclick/humanclicker.py
@@ -1,4 +1,5 @@
 import pyautogui
+import math as Math
 import random
 from pyclick.humancurve import HumanCurve
 
@@ -32,6 +33,23 @@ class HumanClicker():
         random_x = random.randint(x, x + w)
         random_y = random.randint(y, y + h)
         self.move_to_point((random_x, random_y), duration)
+
+    def move_to_ellipse(self, ellipseOriginPoint, width, height, doRandomScale=True, duration=2, humanCurve=None):
+        # Randomise the scale of the ellipse (between 0x and 1x scale)
+        w = width
+        h = height
+        if doRandomScale == True:
+            random_scale_multiplier = random.random()
+            w = width * random_scale_multiplier
+            h = height * random_scale_multiplier
+        # Randomise an angle in radians between 0 and 2pi
+        angle = Math.tau * random.random()
+        # Find x and y positions on the ellipse at the given angle
+        x = (w*h*Math.sin(angle)) / Math.sqrt((h*Math.cos(angle))**2 + (w*Math.sin(angle))**2)
+        y = (w*h*Math.cos(angle)) / Math.sqrt((h*Math.cos(angle))**2 + (w*Math.sin(angle))**2)
+        # Calculate final mouse point and move to that point
+        toPoint = (int(round(x + ellipseOriginPoint[0])), int(round(y + ellipseOriginPoint[1])))
+        self.move_to_point(toPoint, duration)
 
     def click(self, button="left", clicks=1, interval=0.15):
         pyautogui.click(button=button, clicks=clicks, interval=interval)
