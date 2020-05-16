58059d091f2aff42970bef246b00055822e51ec1
lonevox <38600896+lonevox@users.noreply.github.com>
2020-05-16 22:43:36 +1200

    Fixed method naming in test_humanclicker.py
    The move methods in test_humanclicker.py were not renamed when the move method in humanclicker.py was renamed, so this fixes that.

--- tests/test_humanclicker.py
+++ tests/test_humanclicker.py
@@ -10,18 +10,18 @@ class TestHumanClicker(unittest.TestCase):
         width, height = pyautogui.size()
         to_point = (width//2, height//2)
         hc = HumanClicker()
-        hc.move(to_point)
+        hc.move_to_point(to_point)
         self.assertTrue(pyautogui.position() == to_point)
 
-    def test_identity_move(self):
+    def test_identity_move_to_point(self):
         to_point = pyautogui.position()
         hc = HumanClicker()
-        hc.move(to_point)
+        hc.move_to_point(to_point)
         self.assertTrue(pyautogui.position() == to_point)
 
-    def test_random_move(self):
+    def test_random_move_to_point(self):
         width, height = pyautogui.size()
         to_point = random.randint(width//2,width-1), random.randint(height//2,height-1)
         hc = HumanClicker()
-        hc.move(to_point)
+        hc.move_to_point(to_point)
         self.assertTrue(pyautogui.position() == to_point)
